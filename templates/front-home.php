ebb14bd82dcfe5c98d564003b0689e867477278e
makklaud2 <makklaudtwo@gmail.com>
2023-02-22 02:07:52 +0200

    about section start

--- templates/front-home.php
+++ templates/front-home.php
@@ -82,34 +82,34 @@ get_header('front'); ?>
         <div class="about__wrapper">
             <div class="about__title h1">Хто ми? </div>
             <p class="about__text">Eco-bud building group - компанія майбутнього, яка поєднує в собі досвід, креативне мислення та інновації. Компанія стрімко розвивається, тим самим здійснюючи вагомий внесок в розвиток будівельного бізнесу в Україні.</p>
-            <div class="about__subtitle h2"></div>
+            <div class="about__subtitle h2">Наша команда</div>
             <div class="about__photo">
                 <div class="about__photo-item">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/pavlo.png" alt="" class="about__photo-img">
-                    <div class="about__photo-title">Павло Сомов</div>
+                    <div class="about__photo-title h4">Павло Сомов</div>
                     <div class="about__photo-text">Зовниаснк групи компаній Ecobud</div>
                 </div>
                 <div class="about__photo-item">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/marya.png" alt="" class="about__photo-img">
-                    <div class="about__photo-title">Павло Сомов</div>
+                    <div class="about__photo-title h4">Павло Сомов</div>
                     <div class="about__photo-text">Зовниаснк групи компаній Ecobud</div>
                 </div>
             </div>
-            <div class="about__subtitle">Наші принципи</div>
+            <div class="about__subtitle h3">Наші принципи</div>
             <div class="about__principles">
                 <div class="about__principles-item">
                     <div class="about__principles-round"><img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/home.svg" alt="" class="about__principles-logo"></div>
-                    <div class="about__principles-title">Lorem Ipsum</div>
+                    <div class="about__principles-title h3">Lorem Ipsum</div>
                     <p class="about__principles-text">Lorem ipsum dolor sit amet consectetur. Nunc aliquet netus nulla quam. Id ultrices scelerisque nulla amet faucibus habitant mauris scelerisque. Vitae purus nunc turpis odio tristique nisl nisl sed dui. Amet euismod massa porttitor integer hendrerit.</p>
                 </div>
                 <div class="about__principles-item">
                     <div class="about__principles-round"><img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/home.svg" alt="" class="about__principles-logo"></div>
-                    <div class="about__principles-title">Lorem Ipsum</div>
+                    <div class="about__principles-title h3">Lorem Ipsum</div>
                     <p class="about__principles-text">Lorem ipsum dolor sit amet consectetur. Nunc aliquet netus nulla quam. Id ultrices scelerisque nulla amet faucibus habitant mauris scelerisque. Vitae purus nunc turpis odio tristique nisl nisl sed dui. Amet euismod massa porttitor integer hendrerit.</p>
                 </div>
                 <div class="about__principles-item">
                     <div class="about__principles-round"><img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/home.svg" alt="" class="about__principles-logo"></div>
-                    <div class="about__principles-title">Lorem Ipsum</div>
+                    <div class="about__principles-title h3">Lorem Ipsum</div>
                     <p class="about__principles-text">Lorem ipsum dolor sit amet consectetur. Nunc aliquet netus nulla quam. Id ultrices scelerisque nulla amet faucibus habitant mauris scelerisque. Vitae purus nunc turpis odio tristique nisl nisl sed dui. Amet euismod massa porttitor integer hendrerit.</p>
                 </div>
             </div>
