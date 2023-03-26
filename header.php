2bfb8d2d08182b30d9fafbda6e8745f73b387da8
ateslya <ateslya@crispwp.com>
2023-03-26 04:12:37 +0300

    add back header

--- header.php
+++ header.php
@@ -73,9 +73,93 @@
 
     </script>
 </div>
+<?php if (pll_current_language('slug') != 'en') : $current_lang = pll_current_language('slug'); endif; ?>
 <div class="wrapper">
 <header class="header" id="header">
-
+    <div class="header__wrapper">
+        <div class="header__head">
+            <div class="container">
+                <div class="header__head-wrapper">
+                    <a href="/<?php echo $current_lang; ?>" class="header__logo">
+                        <?php $logoHeader = get_field('logo', 'option');
+                            if (!empty($logoHeader)) {?>
+                                <img src="<?php echo $logoHeader['url']; ?>" alt="<?php echo $logoHeader['alt']; ?>" class="header__logo-logo">
+                            <?php } else { ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Header Logo" class="header__logo-logo">
+                        <?php } ?>
+                    </a>
+                    <div class="header__contacts">
+                        <?php if($phones = get_field('phones', 'option')) : ?>
+                            <div class="header__telephone">
+                                <?php $i = 0; foreach ($phones as $item) : ?>
+                                    <?php if($i==0) : ?>
+                                        <a href="tel:<?php echo mb_strtolower(preg_replace('/[^0-9+]/', '', $item['phone']))?>" class="header__telephone-number" target="_blank">
+                                            <?php echo $item['phone'] ;?>
+                                        </a>
+                                    <?php endif; ?>
+                                <?php $i++; endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if($socials = get_field('socials', 'option')) : ?>
+                        <div class="header__icons">
+                            <?php foreach ($socials as $item) : ?>
+                            <a href="<?php echo $item['link'] ;?>" class="header__round">
+                                <span class="icon icon-<?php echo $item['network'] ;?> header__<?php echo $item['network'] ;?>"></span>
+                            </a>
+                            <?php endforeach; ?>
+                        </div>
+                        <?php endif; ?>
+                        <div class="header__lang">
+                            <?php pll_the_languages( array( "display_names_as" => "slug" ) ); ?>
+                        </div>
+                        <div class="burger header__burger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header__nav">
+            <div class="header__burger_mobile burger header__burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <?php if(has_nav_menu( 'primary' )) : ?>
+                <?php wp_nav_menu( array(
+                        'container' => false,
+                        'theme_location' => 'primary',
+                        'menu_class'     => 'header__menu',
+                        'items_wrap'     => '<ul id="%1$s" class="header__menu">%3$s</ul>',
+                    )
+                ); ?>
+            <?endif ; ?>
+            <div class="header__contacts header__contacts_mobile">
+                <?php if($phones = get_field('phones', 'option')) : ?>
+                <div class="header__telephone header__telephone_mobile">
+                    <?php $i = 0; foreach ($phones as $item) : ?>
+                        <?php if($i==0) : ?>
+                            <a href="tel:<?php echo mb_strtolower(preg_replace('/[^0-9+]/', '', $item['phone']))?>" class="header__telephone-number" target="_blank">
+                                <?php echo $item['phone'] ;?>
+                            </a>
+                        <?php endif; ?>
+                    <?php $i++; endforeach; ?>
+                </div>
+                <?php endif ?>
+                <?php if($socials = get_field('socials', 'option')) : ?>
+                    <div class="header__icons header__icons_mobile">
+                        <?php foreach ($socials as $item) : ?>
+                            <a href="<?php echo $item['link'] ;?>" class="header__round">
+                                <span class="icon icon-<?php echo $item['network'] ;?> header__<?php echo $item['network'] ;?>"></span>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </header>
 <main class="main">
 <?php # TODO: code here ?>
