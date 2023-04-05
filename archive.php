dc6614b93e32b24f0d9cc757b3e90a544fd3e911
ateslya <ateslya@crispwp.com>
2023-04-05 20:32:59 +0300

    add back news archive + single

---
+++ archive.php
@@ -0,0 +1,48 @@
+<?php
+get_header();
+
+?>
+<?php if (pll_current_language('slug') != 'en') : $current_lang = pll_current_language('slug'); endif; ?>
+<section class="other-news">
+    <div class="container">
+        <div class="other-news__wrapper">
+            <h1 class="other-news__title h1">
+                <?php if ($current_lang == 'en') {
+                    echo 'News';
+                } else {
+                    echo 'Новини';
+                }?>
+            </h1>
+            <div class="other-news__grid">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php $title = get_the_title();
+                    $img = get_the_post_thumbnail_url();
+                    $link = get_the_permalink();
+                    $subtitle = get_field('subtitle');
+                    ?>
+                    <a href="<?php echo $link;?>" class="other-news__grid-item">
+                        <?php if($img) : ?>
+                            <img src="<?php echo $img;?>" alt="post image" class="other-news__grid-img">
+                        <?php endif;?>
+                        <div class="other-news__grid-content">
+                            <div class="other-news__grid-title h1"><?php echo $title;?></div>
+                            <?php if($subtitle) : ?>
+                                <div class="other-news__grid-subtitle"><?php echo $subtitle;?></div>
+                            <?php endif;?>
+                        </div>
+                    </a>
+                <?php endwhile;?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+/**
+ * Include footer.php of footer-XXX.php for custom page
+ *
+ * @link        https://codex.wordpress.org/Function_Reference/get_footer
+ */
+get_footer();
+
+?>
--- single.php
+++ single.php
@@ -6,12 +6,52 @@
  * @link        https://codex.wordpress.org/Function_Reference/get_header
  */
 get_header(); ?>
-    <section class="content">
-        <div class="content__wrapper">
+    <section class="portfolio-link">
+        <div class="portfolio-link__wrapper">
+            <?php $img = get_the_post_thumbnail_url(); ?>
+            <div class="portfolio-link__banner">
+                <?php if ($img) { ?>
+                    <img src="<?php echo $img;?>" alt="main banner" class="portfolio-link__banner-img">
+                <?php } ?>
+                <div class="portfolio-link__banner-content">
+                    <h1 class="portfolio-link__banner-title h1"><?php the_title(); ?></h1>
+                    <?php if ($sub_title = get_field('subtitle')) { ?>
+                        <h2 class="portfolio-link__banner-subtitle"><?php echo $sub_title;?></h2>
+                    <?php } ?>
+                </div>
+            </div>
             <div class="container">
-                <h1><?php the_title(); ?></h1>
-                <?php the_post();
-                the_content();?>
+                <h6 class="portfolio-link__date"><?php the_date(); ?></h6>
+                <div class="portfolio-link__content">
+                    <?php the_post();
+                    the_content();?>
+                </div>
+            </div>
+            <div class="container">
+                <?php if ($title = get_field('title')) { ?>
+                    <div class="portfolio-link__title h1"><?php echo $title;?></div>
+                <?php } ?>
+                <div class="portfolio-link__content">
+                    <?php if ($items = get_field('items')) { ?>
+                        <?php foreach ($items as $item) : ?>
+                            <div class="portfolio-link__content-item">
+                                <?php if($img = $item['img']) : ?>
+                                    <img src="<?php echo $img['url'];?>" alt="<?php echo $img['alt'];?>" class="portfolio-link__content-img">
+                                <?php endif ; ?>
+                                <div class="portfolio-link__content-descr">
+                                    <?php if($title = $item['title']) : ?>
+                                        <div class="portfolio-link__content-title h3"><?php echo $title;?></div>
+                                    <?php endif ; ?>
+                                    <?php if($description = $item['description']) : ?>
+                                        <div class="portfolio-link__content-text">
+                                            <?php echo $description;?>
+                                        </div>
+                                    <?php endif ; ?>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </section>
